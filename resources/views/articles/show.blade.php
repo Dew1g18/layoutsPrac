228df6cf0f6b74f0bb7ff6e0c4a30a41b9e9db8a
Dew1g18 <Davewadd3@gmail.com>
2020-07-27 17:28:15 +0100

    Show blade and .env, hopefully a pull from linux will allow me to artisan serve and get working from there

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,5 +1,13 @@
 @extends('layout')
 
 @section('content')
+    <div>
+        <ul class="style1">
+            <li class="first">
+                <h3>{{$article->title}}</h3>
+                <p><a href="#">{{$article->body}}</a></p>
+            </li>
+        </ul>
+    </div>
 
 @endsection
